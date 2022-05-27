d56f88d6fda0de624cfab87e3c217beb2a184f1f
AH <andyhop@hotmail.co.uk>
2022-05-27 17:55:53 +0100

    Updating fib with sequence print method

--- src/main/java/com/sparta/ah/FibonacciPositionFinder.java
+++ src/main/java/com/sparta/ah/FibonacciPositionFinder.java
@@ -15,6 +15,31 @@ public class FibonacciPositionFinder {
                 return getPosition(positionToFind - 1) + getPosition(positionToFind - 2);
             }
         }
+
+    public static String getFibonacciSequence(int position)
+    {
+        int num1 = 0, num2 = 1;
+
+        int counter = 0;
+        String sequence ="";
+        // Iterate till counter is position
+        while (counter < position) {
+
+           sequence = num1 + " ";
+
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
+        }
+        return sequence;
     }
+
+
+
+
+    }
+
 
 
